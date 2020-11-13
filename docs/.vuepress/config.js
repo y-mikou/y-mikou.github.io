6e87bb0b58da898873e8c81276764cb5dec4d521
y-mikou <y_mikou@hotmail.com>
2020-11-14 01:24:04 +0900

    wakarann!!!!!!!

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: "あれ",
+    themeConfig: {
+      description: "ディスクリプション",
+      nav: [
+        { text: "Home", link: "/" } // ここはnav barのメニュー表示/aboutページは後ほど
+      ]
+    }
+  };
