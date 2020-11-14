045b032e99db57ae2030836998803dbd8c4db9a7
y-mikou <y_mikou@hotmail.co>
2020-11-15 00:12:11 +0900

    グローバルナビゲータを修正

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,9 @@ module.exports = {
     themeConfig: {
       description: "MonologueStation",
       nav: [
-        { text: "Home", link: "/" },
+        { text: "🏠Home", link: "/" },
+        { text: "📖Literary", link: "/Literary/literary_index.html" },
+        { text: "🖍Doodling", link: "/Doodling/doodling_index.html" },
         { text: "twitter", link: "https://twitter.com/y_mikou" },
         { text: "pixiv", link: "https://www.pixiv.net/users/101579" } 
       ],
--- docs/index.md
+++ docs/index.md
@@ -1,13 +1,13 @@
 ---
-title: 🏠
+title: 🏠HOME
 meta:
-  - name: 🏠
-    content: 🏠
+  - name: 🏠HOME
+    content: 入り口。
 ---
 # [📖Literary](/Literary/literary_index.html "書いたSSとか")
 東方project関連のSSとかです。<br>
 主に「イ゛ぐぅ♥」なやつです。
 
-# [🖍Doodling](/Doodling/doodling_index.html "書いたSSとか")
+# [🖍Doodling](/Doodling/doodling_index.html "描いた落書きとか")
 なんか落書きとかです。<br>
 主にリグルくん。
