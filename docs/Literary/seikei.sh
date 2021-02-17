9ce2af4a9cae5e811a477352abc350e8fc375924
y-mikou <y_mikou@hotmail.com>
2021-02-18 00:22:02 +0900

    5本整形、スクリプトを修正

--- docs/Literary/seikei.sh
+++ docs/Literary/seikei.sh
@@ -5,16 +5,19 @@
 tgtFile=$1                                                                                      #引数で指定されたファイルを対象とする
 destFile=${tgtFile/".md"/"2.md"}                                                                #末尾に2をつけたファイルへ整形して出力する
 touch $destFile                                                                                 #出力先ファイルを生成
-sed '/^$/d' $tgtFile > $destFile                                                                #空行を削除し、別ファイルへ。以降は別ファイルを操作する
+sed       '/^$/d' $tgtFile > $destFile                                                          #空行を削除し、別ファイルへ。以降は別ファイルを操作する
 sed -i -z 's/\(\&emsp\;[^\n]\+\)\n/\1<\/br>\n/g' $destFile                                      #行頭全角スペースで始まる末尾に</br>タグを付与
 sed -i -z 's/\([」）]\)/\1<\/br>/g' $destFile                                                   #」か）で終わる行の末尾に</br>タグを付与
+sed -i    's/\(—\{1,2\}.\+\)/\1<\/br>/g' $destFile                                              #―で始まる行の末尾に</br>タグを付与
+sed -i -z 's/—\{2\}/——/g' $destFile                                                             #3つ以上の―を2つに集約
+sed -i -z 's/\(——[^<\/br>]\+<\/br>\)\n\(&emsp;\)/\1\n\n\2/g' $destFile                          #―で始まる行の次の行が地の文の場合、空行を挿入する
 sed -i -z 's/<div class="info">\n*「\([^\n]\+\)」/<div class="info">\n\1/g' $destFile           #info内の作品名称から「」を削除
 sed -i -z 's/<div class="info">\n\+\([^\n]\+\)\n\+\([^\n]\+\)\n\+\([^\n]\+\)\n\+<\/div>/<div class="info">\n\1<\/br>\n\2<\/br>\n\3<\/br>\n<\/div>\n\n---\n\n<\/br>\n<\/br>\n<\/br>\n/g' $destFile
                                                                                                 #info内整形
 sed -i -z 's/\([」）]\)<\/br>\n&emsp;/\1<\/br>\n\n\&emsp;/g' $destFile                          #」か）で改行して全角スペースとなる境界に空行を挿入する
 sed -i -z 's/\([^」）]<\/br>\)\n\([「（]\)/\1\n\n\2/g' $destFile                                #」でも）でもない文字で改行し「で始まる行との境界に空行を挿入する
-sed -i -z 's/[^\n]\n\(—\+[^\n]\+\)\n/\n\n\1\n/g' $destFile                                      #―で始まる行の前に空行を挿入する
-sed -i -z 's/^\(—\+[^<\/br>\n]+\)\n\+\([「（]\)/\1<\/br>\n\2/g' $destFile                       #―で始まる行の前が」か）だった場合、空行を削除する
+sed -i -z 's/[^\n]\n\(—\+[^\n]\+\)\n/\n\n\1\n/g' $destFile                                      #―で始まる行の前が空行でない場合、空行を挿入する
+sed -i -z 's/^\(—\+[^(<\/br>)\n]\+\)\n\+\([^「（]\)/\1<\/br>\n\n\2/g' $destFile                 #―で始まる行の後ろが「でも（でもない場合、空行を追加する
 sed -i -z 's/<div class=\"afterword\">/\n<\/br>\n<\/br>\n<\/br>\n\n---\n\n\#\# 後記・コメント\n<details>\n<summary>あとがき<\/summary>/g' $destFile
                                                                                                 #後記・コメント領域の整形
 sed -i -z 's/【\([0-9]\)/\n【\1/g' $destFile                                                    #コメントを一人毎に空行挿入する
@@ -36,4 +39,4 @@ sed -i -z 's/<div class=\"info\">\n\+\([^\n]\+\)<\/br>/---\ntitle: \1\nsidebar: 
 sed -i -z 's/<\/summary>\n<\/br>\n\+<\/br>/<\/summary>\n/g' $destFile                           #</summary>のあとに無用な改行がある場合削除
 sed -i -z 's/\(<\/br>\)\+/<\/br>/g' $destFile                                                   #</br>タグが改行を挟まず連続している場合、一つに集約
 sed -i -z 's/\(title: [^<]\+\)<\/br>/\1/g' $destFile                                            #タイトル末尾に</br>タグがどっかでついてしまっているので除去
-sed -i -z 's/\([！？]\)\([^　！？」）—<]\)/\1　\2/g' $destFile                                            #タイトル末尾に</br>タグがどっかでついてしまっているので除去
+sed -i -z 's/\([！？]\)\([^　！？」）—<\/br>\n]\)/\1　\2/g' $destFile                           #！？のあとに全角スペースが含まれていない場合追加。ただし直後が特定の文字の場合は入れない
